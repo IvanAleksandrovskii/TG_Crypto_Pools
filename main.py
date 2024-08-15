1a2c901322cd97ba128a6d4ef068a5cb930eb6dc
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-15 11:34:15 +0700

    api base structure added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import uvicorn
 
 from core.models import db_helper
 from core import settings, logger
+from api import api_router
 
 ic.disable()
 # ic.enable()
@@ -29,6 +30,8 @@ main_app = FastAPI(
     lifespan=lifespan,
     default_response_class=ORJSONResponse,
 )
+
+main_app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == '__main__':
     uvicorn.run(ic("main:main_app"),
