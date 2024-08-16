0230210d0f766a50eddd172b565c39f03d83e1ff
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-16 11:58:15 +0700

    checkboxes relations picks added

--- core/admin/models/chain.py
+++ core/admin/models/chain.py
@@ -1,10 +1,13 @@
-from starlette.requests import Request
-from wtforms import validators
+from sqlalchemy import select
 from sqlalchemy.orm import joinedload
+from starlette.requests import Request
+from wtforms import validators, SelectMultipleField
+from wtforms.widgets import ListWidget, CheckboxInput
 
-from core.models import Chain
+from core.models import Chain, Coin
 from core import logger
 from .base import BaseAdminModel
+from core.admin import async_sqladmin_db_helper
 
 
 class ChainAdmin(BaseAdminModel, model=Chain):
@@ -32,6 +35,49 @@ class ChainAdmin(BaseAdminModel, model=Chain):
 
     def search_query(self, stmt, term):
         return stmt.filter(Chain.name.ilike(f"%{term}%"))
+
+    async def scaffold_form(self):
+        form_class = await super().scaffold_form()
+        form_class.coins = SelectMultipleField(
+            'Coins',
+            choices=await self._get_coin_choices(),
+            widget=ListWidget(prefix_label=False),
+            option_widget=CheckboxInput(),
+            coerce=self._coerce_coin
+        )
+        return form_class
+
+    def _coerce_coin(self, value):
+        if hasattr(value, 'id'):
+            return str(value.id)
+        return str(value)
+
+    async def _get_coin_choices(self):
+        async with async_sqladmin_db_helper.session_getter() as session:
+            result = await session.execute(select(Coin).where(Coin.is_active == True))
+            coins = result.scalars().all()
+            return [(str(coin.id), f"{coin.name} ({coin.code})") for coin in coins]
+
+    async def get_one(self, _id):
+        async with async_sqladmin_db_helper.session_getter() as session:
+            stmt = select(self.model).options(
+                joinedload(Chain.coins)
+            ).filter_by(id=_id)
+            result = await session.execute(stmt)
+            return result.scalar_one_or_none()
+
+    async def edit_form(self, obj):
+        form = await super().edit_form(obj)
+        if obj and obj.coins:
+            form.coins.data = [str(coin.id) for coin in obj.coins]
+        return form
+
+    async def _update_model_fields(self, session, model, data):
+        await super()._update_model_fields(session, model, data)
+        if 'coins' in data:
+            coin_ids = data['coins']
+            coins = await session.execute(select(Coin).where(Coin.id.in_(coin_ids)))
+            model.coins = coins.scalars().all()
 
     async def after_model_change(self, data: dict, model: Chain, is_created: bool, request: Request) -> None:
         action = "Created" if is_created else "Updated"
--- core/admin/models/coin.py
+++ core/admin/models/coin.py
@@ -1,14 +1,15 @@
-from sqlalchemy import or_
+from sqlalchemy import or_, select
 from sqlalchemy.orm import joinedload
 from starlette.requests import Request
-from wtforms import validators
+from wtforms import validators, SelectMultipleField
+from wtforms.widgets import ListWidget, CheckboxInput
 
-from core.models import Coin
+from core.models import Coin, Chain
 from core import logger
 from .base import BaseAdminModel
+from core.admin import async_sqladmin_db_helper
 
 
-# TODO: add coin symbol field (?)
 class CoinAdmin(BaseAdminModel, model=Coin):
     column_list = [Coin.id, Coin.name, Coin.code, 'chains', 'pools', Coin.is_active]
     column_formatters = {
@@ -36,6 +37,49 @@ class CoinAdmin(BaseAdminModel, model=Coin):
 
     def search_query(self, stmt, term):
         return stmt.filter(or_(Coin.name.ilike(f"%{term}%"), Coin.code.ilike(f"%{term}%")))
+
+    async def scaffold_form(self):
+        form_class = await super().scaffold_form()
+        form_class.chains = SelectMultipleField(
+            'Chains',
+            choices=await self._get_chain_choices(),
+            widget=ListWidget(prefix_label=False),
+            option_widget=CheckboxInput(),
+            coerce=self._coerce_chain
+        )
+        return form_class
+
+    def _coerce_chain(self, value):
+        if hasattr(value, 'id'):
+            return str(value.id)
+        return str(value)
+
+    async def _get_chain_choices(self):
+        async with async_sqladmin_db_helper.session_getter() as session:
+            result = await session.execute(select(Chain).where(Chain.is_active == True))
+            chains = result.scalars().all()
+            return [(str(chain.id), chain.name) for chain in chains]
+
+    async def get_one(self, _id):
+        async with async_sqladmin_db_helper.session_getter() as session:
+            stmt = select(self.model).options(
+                joinedload(Coin.chains)
+            ).filter_by(id=_id)
+            result = await session.execute(stmt)
+            return result.scalar_one_or_none()
+
+    async def edit_form(self, obj):
+        form = await super().edit_form(obj)
+        if obj and obj.chains:
+            form.chains.data = [str(chain.id) for chain in obj.chains]
+        return form
+
+    async def _update_model_fields(self, session, model, data):
+        await super()._update_model_fields(session, model, data)
+        if 'chains' in data:
+            chain_ids = data['chains']
+            chains = await session.execute(select(Chain).where(Chain.id.in_(chain_ids)))
+            model.chains = chains.scalars().all()
 
     async def after_model_change(self, data: dict, model: Coin, is_created: bool, request: Request) -> None:
         action = "Created" if is_created else "Updated"
