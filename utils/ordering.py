1e5ed2427c99e37eb01dc46864e48e5d65245c54
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-09-30 22:58:17 +0700

    ordering global fix

--- utils/ordering.py
+++ utils/ordering.py
@@ -1,9 +1,10 @@
 from typing import List, Optional, Any, Type
+
 from fastapi import Query
-from sqlalchemy import desc, asc, cast, BigInteger
+
+from sqlalchemy import desc, asc, cast, BigInteger, Float, Integer, Numeric
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.sql.expression import nullslast
-
 
 class Ordering:
     def __init__(self, model: Type[DeclarativeBase], allowed_fields: List[str], default_field: str = "id", default_desc: bool = True):
@@ -11,17 +12,36 @@ class Ordering:
         self.allowed_fields = allowed_fields
         self.default_field = default_field
         self.default_desc = default_desc
+        self.numeric_types = (BigInteger, Integer, Float, Numeric)
+        self._init_field_types()
+
+    def _init_field_types(self):
+        self.field_types = {}
+        for field in self.allowed_fields:
+            column = getattr(self.model, field)
+            self.field_types[field] = column.type
+
+    def _get_cast_column(self, field: str, column: Any) -> Any:
+        field_type = self.field_types.get(field)
+        if isinstance(field_type, self.numeric_types):
+            if isinstance(field_type, BigInteger):
+                return cast(column, BigInteger)
+            elif isinstance(field_type, Integer):
+                return cast(column, Integer)
+            elif isinstance(field_type, Float):
+                return cast(column, Float)
+            elif isinstance(field_type, Numeric):
+                return cast(column, Numeric)
+        return column
 
     def order_by(self, order: Optional[str] = Query(None), order_desc: Optional[bool] = None) -> Any:
         if order and order in self.allowed_fields:
             column = getattr(self.model, order)
-            if order == 'audience':
-                column = cast(column, BigInteger)
+            column = self._get_cast_column(order, column)
             direction = desc if order_desc else asc
         else:
             column = getattr(self.model, self.default_field)
-            if self.default_field == 'audience':
-                column = cast(column, BigInteger)
+            column = self._get_cast_column(self.default_field, column)
             direction = desc if self.default_desc else asc
 
         return nullslast(direction(column))
